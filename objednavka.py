2fdd7938c6bb3be6dacceaaf61038f0e64637b0c
kuskovam3-rgb <kuskova.m3@gmail.com>
2026-02-21 15:13:58 +0100

    Create objednavka.py for restaurant orders
    Added a basic restaurant ordering script with user input.

---
+++ objednavka.py
@@ -0,0 +1,20 @@
+print("Vítejte!\nVítejte v naší restauraci!")
+name = input("Jaké je vaše jméno? \n")
+print (name)
+
+
+# Vytvoř chatbota!
+
+# Úkol - Přidej svůj kód!
+# nabídněte zákaznikovi menu (print funkce)
+# zákazník si vybere jedno z jídel /nápojů (input funkce)
+# potvrďte zákazníkovi objednávku
+# můžete se zeptat na adresu, telefonní číslo, je to jen na vás ... - využijte tak svých znalostí proměnných, print a input funkce
+
+# Kód nefunguje?
+# nechybí ti uvozovky či závorky?
+# máš příkaz print malými písmeny?
+# máš víceslovný název proměnné oddělen podtržítkem?
+# dal jsi za vstup v konzoli Enter?
+# nezapomněl jsi na + při sčítání řetězců?
+# přiřadil jsi proměnnou k výsledku input funkce?
