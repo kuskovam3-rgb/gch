9ef70ba39d67c1216c49627a28a0b3834d6a8b06
kuskovam3-rgb <kuskova.m3@gmail.com>
2026-02-21 07:32:40 +0100

    Add song.py with repeated print statements

---
+++ song.py
@@ -0,0 +1,13 @@
+# Proměnná
+
+print("Hallo, halo, halo, how low?")
+print("Hallo, halo, halo, how low?")
+print("Hallo, halo, halo, how low?")
+print("Hello, hello, hello...")
+print("")
+
+song = "Hello,hello,hello, how low?"
+
+print(song)
+print(song)
+print(song)
