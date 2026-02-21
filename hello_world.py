d4c47503db5b346b9f61ab2745537d68dd84d477
kuskovam3-rgb <kuskova.m3@gmail.com>
2026-02-21 10:26:34 +0100

    Create hello_world.py with print statement and tasks
    Added a simple Python script that prints 'Hello world!' and includes tasks for exploring the code.

---
+++ hello_world.py
@@ -0,0 +1,24 @@
+print("Hello world!")
+
+
+
+
+
+##### Úkol - Prozkoumej kód
+
+
+# Jaká část příkazu na řádku 1 je řetězec?
+ # Odpověď:
+
+# Co vytiskne příkaz na řádku 1 do konzole?
+ # Odověď: 
+
+# Co by se stalo, pokud bych v příkazu na řádku 1 smazala závorku na konci?
+ # Odpověď: 
+
+ ##### Úkol – Přidej svůj kód
+
+# - na řádek 2, 3, 4 přidej svůj vlastní kód
+# - pomocí print funkce vytiskni do mkonzole své jméno, věk, záliby
+# - pomocí funkce print vytiskni prázdný řádek
+# - na další řádek vytiskni text, kterým se rozloučíš
