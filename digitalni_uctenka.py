60b68b058375c21508896dd9c7fa1777111aceaa
kuskovam3-rgb <kuskova.m3@gmail.com>
2026-02-22 19:49:30 +0100

    Create digitalni_uctenka.py for receipt generation
    Added a script for generating a digital receipt from a café purchase.

---
+++ digitalni_uctenka.py
@@ -0,0 +1,34 @@
+# Digitální účtenka z Kavárny
+
+# Vytvoř program, který se uživatele zeptá na detaily nákupu, a pak je přehledně vypíše ve formě účtenky.
+
+# 1. Část: Sběr dat (Input)
+# Naprogramuj robota, aby se postupně zeptal na tyto informace:
+
+# Jméno obsluhy (kdo účtenku vystavil).
+
+# Název nápoje (např. Espresso, Latte).
+
+# Název něčeho k jídlu (např. Dort, Croissant).
+
+# Cena za pití.
+
+# Cena za jídlo.
+
+# 2. Část: Výpis účtenky (Print)
+# Pomocí funkcí print a spojování textu + vytvoř výstup, který vypadá jako opravdový lístek z pokladny. Použij pomlčky - nebo hvězdičky * k vytvoření čar.
+
+
+
+# *******************************
+#        KAVÁRNA U PYTHONU       
+# *******************************
+# Obsluha: Petr
+# -------------------------------
+# Položka: Latte ....... 65 Kč
+# Položka: Croissant ... 45 Kč
+# -------------------------------
+# CELKEM: 110 Kč
+# -------------------------------
+#     Děkujeme za návštěvu!
+# *******************************
