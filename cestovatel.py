c4262bc874eeaa2131383ac1714e68e5b12cc166
kuskovam3-rgb <kuskova.m3@gmail.com>
2026-02-22 13:48:42 +0100

    Add initial structure for cestovatel.py

---
+++ cestovatel.py
@@ -0,0 +1,10 @@
+jmeno = input("Jak se jmenuješ? ")
+
+
+
+# 1. Zeptej se uživatele na jeho jméno a ulož ho do proměnné 'jmeno'
+# 2. Zeptej se na zemi, kterou chce navštívit, a ulož ji do proměnné 'destinace'
+# 3. Zeptej se na počet dní, které tam chce strávit, a ulož to do 'pocet_dni'
+# 4. Vypiš větu ve stylu: "Dobrodruh [jmeno] se chystá do země [destinace] na [pocet_dni] dní!"
+
+# Nezapomeň na mezery v uvozovkách, aby se slova neslepila!
