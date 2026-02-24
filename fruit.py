2d4a6d54e50b5d76eb865fb17586710df890fd14
kuskovam3-rgb <kuskova.m3@gmail.com>
2026-02-24 12:28:40 +0100

    Create fruit.py with fruit and ice cream examples
    Added fruit.py with examples of variable assignment and print statements.

---
+++ fruit.py
@@ -0,0 +1,38 @@
+fruit = "apple"
+best_fruit = fruit
+fruit = "banana"
+
+print(best_fruit)
+best_icecream = "vanilla"
+worst_icecream = "chocolate"
+print(best_icecream)
+print(worst_icecream)
+
+
+
+
+
+
+
+##### Úkol - Prozkoumej kód
+
+# Co by se stalo, pokud bych na řádku 1 zapoměla řetězec ohraničit uvozovkami?   
+ # Odpověď:  chybová hláška, chyba: name error
+
+# Co by se stalo, pokud bych na řádku 5 chtěla vytisknout Best_fruit místo best_fruit?
+ # Odpověď:  chybová hláška, chyba: Name error
+
+# Proč je výstupem příkazu na řádku 5 apple a ne banana?
+ # Odpověď: Na druhém řádku se proměnná best_fruit přiřadila hodnotě apple a poté se již nezměnila.
+
+
+
+#  Proměnné
+
+# Úkol - Přidej svůj kód!!
+    
+    # na řádcích 7 a 8 vytvoř nové proměnné, do kterých přiřadíš svoji nejvíce a nejméně oblíbenou zmrzlinu
+
+    # proměnné pojmenuj smysluplně a nejlépe použijte angličtinu
+
+    # na řádky 9 a 10 pak vytiskni obě proměnné
