4c87d8558421056e228b6c6cc0bbeab13fbec5d0
kuskovam3-rgb <kuskova.m3@gmail.com>
2026-02-26 09:29:43 +0100

    Refactor fruit.py with tasks and comments
    Updated fruit.py with comments and tasks in Czech.

--- fruit.py
+++ fruit.py
@@ -1,38 +1,29 @@
-fruit = "apple"
-best_fruit = fruit
-fruit = "banana"
+# --- 1. ČÁST: ANALÝZA (Sleduj, co se děje s hodnotami) ---
+ovoce = "jablko"
+nejlepsi_ovoce = ovoce  # Do 'nejlepsi_ovoce' se kopíruje aktuální hodnota ("jablko")
+ovoce = "banan"         # Původní proměnnou měníme, ale kopie zůstává stejná
 
-print(best_fruit)
-best_icecream = "vanilla"
-worst_icecream = "chocolate"
-print(best_icecream)
-print(worst_icecream)
+# ÚKOL 1: Vytiskni proměnnou 'nejlepsi_ovoce'. Co se objeví v konzoli?
 
 
 
+# ÚKOL 2. ČÁST: PRÁCE S CHYBAMI  ---
+# Zamysli se (případně vyzkoušej) a napiš, co by se stalo, kdyby:
+# A) Zapomeneš uvozovky: nejlepsi_zmrzlina = vanilka
+Odpověď:
+# B) Napíšeš špatně název: print(Best_fruit) místo best_fruit
+Odpověď:
+
+
+# ÚKOL 3. ČÁST: TVŮJ VLASTNÍ KÓD (Zmrzlinový bar) ---
+# Instrukce:
+# 1. Vytvoř proměnnou pro svou nejoblíbenější zmrzlinu (použij anglický název)
+# 2. Vytvoř proměnnou pro svou nejméně oblíbenou zmrzlinu
+# 3. Obě proměnné pojmenuj smysluplně (např. best_ice_cream)
 
 
 
 
-##### Úkol - Prozkoumej kód
-
-# Co by se stalo, pokud bych na řádku 1 zapoměla řetězec ohraničit uvozovkami?   
- # Odpověď:  chybová hláška, chyba: name error
-
-# Co by se stalo, pokud bych na řádku 5 chtěla vytisknout Best_fruit místo best_fruit?
- # Odpověď:  chybová hláška, chyba: Name error
-
-# Proč je výstupem příkazu na řádku 5 apple a ne banana?
- # Odpověď: Na druhém řádku se proměnná best_fruit přiřadila hodnotě apple a poté se již nezměnila.
 
 
 
-#  Proměnné
-
-# Úkol - Přidej svůj kód!!
-    
-    # na řádcích 7 a 8 vytvoř nové proměnné, do kterých přiřadíš svoji nejvíce a nejméně oblíbenou zmrzlinu
-
-    # proměnné pojmenuj smysluplně a nejlépe použijte angličtinu
-
-    # na řádky 9 a 10 pak vytiskni obě proměnné
