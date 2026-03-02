c843f33fb3609a5067b0250b1a6e6ed60fd06823
kuskovam3-rgb <kuskova.m3@gmail.com>
2026-03-02 07:39:05 +0100

    Revise security protocol instructions and structure

--- protokol_bunkr.py
+++ protokol_bunkr.py
@@ -1,43 +1,27 @@
 # ==========================================================
 # PROJEKT: ZABEZPEČENÍ BUNKRU 51
-# Zadání: Dokonči bezpečnostní protokol podle instrukcí níže.
+# Zadání: Vytvoř bezpečnostní protokol pro vstup do základny.
 # ==========================================================
 
-# --- 1. ČÁST: GRAFICKÉ ZÁHLAVÍ ---
-# Použij proměnnou a násobení řetězců k vytvoření oddělovače
-cara = "-" * 45
-
-print(cara)
-print("!!! VAROVÁNÍ: VSTUPUJETE DO ZAKÁZANÉ OBLASTI !!!")
-print(cara)
+# 1. ČÁST: GRAFICKÉ ZÁHLAVÍ
+# Vytvoř proměnnou 'cara', která bude obsahovat 40 pomlček.
+# Vytiskni čáru, pod ní nápis "VSTUP DO BUNKRU" a pak znovu čáru.
 
 
-# --- 2. ČÁST: IDENTIFIKACE ---
-# ÚKOL: Získej od uživatele jméno a ulož ho do proměnné 'jmeno'
-jmeno = input("IDENTIFIKUJTE SE (Zadejte jméno): ")
+# 2. ČÁST: IDENTIFIKACE
+# Zeptej se uživatele na jeho jméno a ulož ho do proměnné 'jmeno'.
+# Zeptej se na přístupové heslo a ulož ho do proměnné 'heslo'.
 
 
-# --- 3. ČÁST: AUTORIZAČNÍ KÓD ---
-# ÚKOL: Získej od uživatele kód. 
-# V otázce (v inputu) vypiš jeho jméno VELKÝMI PÍSMENY (použij .upper())
-# Příklad: "ZADEJTE PŘÍSTUPOVÝ KÓD, BILLY: "
-
-kod = input("ZADEJTE PŘÍSTUPOVÝ KÓD, " + jmeno.upper() + ": ")
+# 3. ČÁST: DOPLŇUJÍCÍ ÚDAJE
+# Zeptej se na aktuální čas (např. 14:30) a ulož ho do proměnné 'cas'.
 
 
-# --- 4. ČÁST: POTVRZENÍ PŘÍSTUPU ---
-# ÚKOL: Vypiš závěrečný protokol.
-# Musí obsahovat oddělovací čáru, potvrzení o ověřování a přivítání se jménem.
+# 4. ČÁST: ZÁVĚREČNÝ PROTOKOL
+# Vypiš potvrzení, které bude vypadat takto:
+# ----------------------------------------
+# PROTOKOL AKTIVOVÁN V: [čas]
+# VÍTEJTE, AGENTE [jméno]!
+# VAŠE HESLO [heslo] BYLO PŘIJATO.
+# ----------------------------------------
 
-print(cara)
-print("PROBÍHÁ OVĚŘOVÁNÍ KÓDU: " + kod)
-print("PŘÍSTUP POVOLEN! Vítejte zpět, " + jmeno + ".")
-print(cara)
-
-
-# ==========================================================
-# Pokračuj dál a přidej další funkce do protokolu:
-# 1. Zeptej se uživatele na aktuální čas a vypiš ho v protokolu.
-# 2. Na úplný konec přidej otázku: "Chcete aktivovat zámek? (ano/ne): "
-#    a výsledek vypiš jako: "STATUS ZÁMKU: [odpověď]"
-# ==========================================================
