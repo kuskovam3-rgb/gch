1b7adf6d13241ced093951422647f9d3d896df1c
kuskovam3-rgb <kuskova.m3@gmail.com>
2026-03-05 10:49:05 +0100

    Implement hotdog purchase program
    Fix syntax errors and improve hotdog purchase logic.

---
+++ chyby_automat.py
@@ -0,0 +1,13 @@
+# --- PROGRAM S CHYBAMI ---
+# Cíl: Koupit si hotdog za 60 Kč
+
+penize = input("Kolik máš u sebe peněz v Kč? ")
+
+if penize > 60
+print("Můžeš si koupit hotdog!")
+zbytek = penize - 60
+print("Zůstane ti " + zbytek + " Kč.")
+elif penize = 60:
+    print("Máš to přesně! Tady je hotdog.")
+else
+    print("Máš málo peněz, hotdog nebude.")
