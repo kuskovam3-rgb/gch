f48bad50a17b4b322169f95870515c210b368657
kuskovam3-rgb <kuskova.m3@gmail.com>
2026-03-05 09:31:12 +0100

    Add smart ticketing system for mountain railway

---
+++ pokladna_horska_draha.py
@@ -0,0 +1,19 @@
+# ==========================================================
+# ÚKOL: CHYTRÁ POKLADNA HORSKÉ DRÁHY (elif)
+# Cíl: Rozdělit návštěvníky do 3 cenových kategorií podle věku.
+# ==========================================================
+
+# 1. ČÁST: VSTUPY
+# Zeptej se na jméno.
+# Zeptej se na věk (nezapomeň na int()!).
+
+
+# 2. ČÁST: PODMÍNKY (Ceník)
+# Nastav pravidla takto:
+# - Pokud je věk méně než 12 let -> cena je 50 Kč.
+# - Pokud je věk mezi 12 a 18 lety -> cena je 100 Kč. (použij elif)
+# - Ve všech ostatních případech -> cena je 150 Kč. (použij else)
+
+
+# 3. ČÁST: VÝPIS
+# Vypiš jméno návštěvníka a výslednou cenu lístku.
